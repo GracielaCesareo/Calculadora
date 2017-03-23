08ef03074df754ca9a2f70f22772105aecc98807
GracielaCesareo <graciela.cesareoc@gmail.com>
2017-03-23 15:56:24 -0600

    calculadora version final, esta version usa el metodo eval

--- javascript.js
+++ javascript.js
@@ -1,3 +1,16 @@
+
+//La funcion Math.abs permite trabajar con los valores absolutos de los numeros
+//Esta funcion comprueba los valores de los numeros ingresados.
+function comprobar() {
+  if (Math.abs(calculadora.display.value)) {
+    return calculadora.display.value;
+  } else {
+    return Math.abs(calculadora.display.value);
+  }
+}
+
+//La funcion eval ejecuta la declaracion entre los argumentos. 
+//Esta funcion toma el valor que arroja cada boton y ejecuta la operacion.
 function resultado() {
   calculadora.display.value = eval(calculadora.display.value);
 }
